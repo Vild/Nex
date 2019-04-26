bde959754fde683d5a3125d7c0c358580b69f882
Dan Printzell <xwildn00bx@gmail.com>
2019-04-26 13:46:01 +0200

    Bakup
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

---
+++ source/app.d
@@ -0,0 +1,10 @@
+module app;
+
+import nex.parse;
+
+void main() {
+	BuildEnvironment be = new BuildEnvironment();
+	be.addBuildFile("tests/base/nex.sdl");
+	//ProjectScope ps = t.parse;
+	//ps.dot();
+}
